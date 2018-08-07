9fa5c59c46f9f5f91b7268b38d43d641a97a3fa9
Guy Konan <kkfguy@gmail.com>
2018-08-07 19:22:41 +0900

    just a comment

--- models-master/samples/core/get_started/premade_estimator.py
+++ models-master/samples/core/get_started/premade_estimator.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+#  more comments ignored by git
 """An Example of a DNNClassifier for the Iris dataset."""
 from __future__ import absolute_import
 from __future__ import division
